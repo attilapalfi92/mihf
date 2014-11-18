eaf0da97f0edbdb6d247f01e670690c4751812cf
brabulla <bulla.adam@gmail.com>
2014-11-18 11:16:30 +0100

    Adedd agent and gui

---
+++ intelligence/src/agent/AgentMain.java
@@ -0,0 +1,7 @@
+package agent;
+
+/**
+ * Created by Adam on 2014.11.18..
+ */
+public class AgentMain {
+}
---
+++ intelligence/src/gui/GuiMain.java
@@ -0,0 +1,7 @@
+package gui;
+
+/**
+ * Created by Adam on 2014.11.18..
+ */
+public class GuiMain {
+}
