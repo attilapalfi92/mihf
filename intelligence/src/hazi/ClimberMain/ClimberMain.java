1f35aebd0a8dcdd2daa67fecce5da63ed6274af4
brabulla <bulla.adam@gmail.com>
2014-11-18 10:56:25 +0100

    Reformatted

---
+++ intelligence/src/hazi/ClimberMain/ClimberMain.java
@@ -0,0 +1,12 @@
+import java.lang.String;
+import java.lang.System;
+
+public class ClimberMain{
+
+    public static void main(String[] args ){
+
+        System.out.println("Hello Atis");
+
+    }
+
+}
