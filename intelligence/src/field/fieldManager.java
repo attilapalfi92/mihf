da9bc3d2f909ddba82ca63fc211c8082e83c30d3
Attila Pálfi <attila.palfi.92@gmail.com>
2014-11-18 11:18:36 +0100

    yeah new class

---
+++ intelligence/src/field/fieldManager.java
@@ -0,0 +1,18 @@
+package field;
+
+/**
+ * Created by Attila on 2014.11.18..
+ */
+public class fieldManager {
+    private double[] field;
+
+    public fieldManager(int X, int Y) {
+        this.field = new double[X*Y];
+    }
+
+    public void generateField()
+    {
+
+    }
+
+}
