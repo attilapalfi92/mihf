deab8e3aa2f0e25f1e67d7f861b9580dc2c45e96
Attila Pálfi <attila.palfi.92@gmail.com>
2014-12-12 21:51:22 +0100

    inetrface2

---
+++ src/beam/SortedList.java
@@ -0,0 +1,23 @@
+package beam;
+
+import field.Field;
+
+/**
+ * Created by Attila on 2014.12.12..
+ */
+public class SortedList implements ISortedList {
+    @Override
+    public void setLimit(int K) {
+
+    }
+
+    @Override
+    public void addField(Field F) {
+
+    }
+
+    @Override
+    public Field getField() {
+        return null;
+    }
+}
